168167ebcec3c906684f22c1e307d3c114f6ad06
Alejandro Ocampo Cisneros <ocampoalejandro.oc@gmail.com>
2020-11-21 22:02:59 -0600

    Commit 2

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,20 +28,61 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.imageBox1 = new Emgu.CV.UI.ImageBox();
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).BeginInit();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(146, 44);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "IMPORTAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(164, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(146, 44);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "ESCALA DE GRISES";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // imageBox1
+            // 
+            this.imageBox1.Location = new System.Drawing.Point(13, 72);
+            this.imageBox1.Name = "imageBox1";
+            this.imageBox1.Size = new System.Drawing.Size(409, 361);
+            this.imageBox1.TabIndex = 2;
+            this.imageBox1.TabStop = false;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(880, 464);
+            this.ClientSize = new System.Drawing.Size(977, 534);
+            this.Controls.Add(this.imageBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
             this.Name = "Form1";
             this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.imageBox1)).EndInit();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private Emgu.CV.UI.ImageBox imageBox1;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -7,14 +7,49 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Emgu;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
 
 namespace HistogramaEscalaGrises
 {
     public partial class Form1 : Form
     {
+        Image<Bgr, byte> MyImage;
+        Image<Gray, byte> GrayImage;
+
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            //Para importar la imagen
+            OpenFileDialog ofd = new OpenFileDialog();
+            if(ofd.ShowDialog()==DialogResult.OK)
+            {
+                MyImage = new Image<Bgr, byte>(ofd.FileName);
+                
+                //Usando un imageBox no necesitamos transformar a bitmap
+                imageBox1.Image = MyImage;
+
+                //Permite guardar hacer zoom con la imagen
+                imageBox1.FunctionalMode = Emgu.CV.UI.ImageBox.FunctionalModeOption.Everything;
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (MyImage!=null)
+            {
+                
+            }else
+            {
+                MessageBox.Show("Debe importar una imagen");
+            }
         }
     }
 }
